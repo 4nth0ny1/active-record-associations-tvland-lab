f312d466100827efcdd30d9025b46e9b63919a5e
4anth0ny1 <acatullo4@gmail.com>
2021-02-27 00:46:21 -0600

    commit tree cc4c9a674f4cdd21d561c225b02acd81d5317855

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,15 +1,18 @@
 class Actor < ActiveRecord::Base
 
     has_many :characters
-    has_many :shows
-
+    has_many :shows, through: :characters
+  
     def full_name
-        Actor.select(first_name).select(last_name)
-    end 
-
+      name = "#{self.first_name} #{self.last_name}"
+    end
+  
     def list_roles
-        self.characters.first
-        self.shows.first
-    end 
-
+      roles = []
+      array_of_characters = self.characters.map {|character| character.name}
+      array_of_shows = self.shows.map {|shows| shows.name}
+      role = array_of_characters.concat array_of_shows
+      roles << role.join(" - ")
+      roles
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,23 @@
 class Show < ActiveRecord::Base
   
     has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+    
+    def actors_list
+        # Actor.full_name        
+        
+        self.actors.map{|act| "#{act.first_name}".concat " #{act.last_name}"}
+        
+        
+    end
+    # def list_roles
+    #     roles = []
+    #     array_of_characters = self.characters.map {|character| character.name}
+    #     array_of_shows = self.shows.map {|shows| shows.name}
+    #     role = array_of_characters.concat array_of_shows
+    #     roles << role.join(" - ")
+    #     roles
+    # end
+
 end
